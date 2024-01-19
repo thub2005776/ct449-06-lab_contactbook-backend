bfeb01aa7817bfc3841d7cc8b98f83f1c32fb754
thub2005776 <thub2005776@student.ctu.edu.vn>
2024-01-19 20:33:54 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const contactsRouter = require('./app/routes/contact.route');
 
 const app = express();
 app.use(cors());
@@ -8,5 +9,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({message : "Welcome to contactbook application."});
 });
+
+app.use('/api/contacts', contactsRouter);
 
 module.exports = app;
